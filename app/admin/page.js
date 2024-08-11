bee1821e3696db85d2a910af5d90dac41e08ef45
Francisco <panchifeijoo@gmail.com>
2024-08-11 18:47:55 -0300

    maquetacion 1.2

---
+++ app/admin/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Admin = () => {
+  return (
+    <div className="h-dvh flex justify-center items-center bg-gray-500">
+      Admin{" "}
+    </div>
+  );
+};
+
+export default Admin;
